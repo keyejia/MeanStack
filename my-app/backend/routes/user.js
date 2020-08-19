9ddda3cf3f18839d57d40c90920e9a07087c63ce
Jia, Kellen (kj393d) <kj393d@att.com>
2020-08-19 18:02:32 -0400

    deleted my-app

--- my-app/backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/user');
-
-router.post("/signup", UserController.createUser);
-
-router.post("/login", UserController.userLogin);
-
-module.exports = router;
